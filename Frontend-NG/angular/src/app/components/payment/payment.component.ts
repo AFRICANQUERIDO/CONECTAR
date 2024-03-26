c4aee65acbd74cd47880fcd55be1437005813afa
Your Name <janengene12@gmail.com>
2024-03-26 22:11:03 +0300

    trying

--- Frontend-NG/angular/src/app/components/payment/payment.component.ts
+++ Frontend-NG/angular/src/app/components/payment/payment.component.ts
@@ -1,19 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PaymentService } from '../../services/payment.service';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Order, orderResponse } from '../../intefaces/gig.interface';
 import { GigsService } from '../../services/gigs.service';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, NavbarComponent],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit{
   paymentForm!: FormGroup;
   orderID!: string;
   order: orderResponse[] = [];
@@ -32,39 +33,44 @@ export class PaymentComponent {
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.orderID = params['orderID'];
-
+      
+      localStorage.setItem('orderID', this.orderID);
+  
       this.orderService.getOrders(this.orderID).subscribe((res: { order: Order[] }) => {
         this.totalAmount = res.order[0].totalAmount;
-
+  
         this.paymentForm = this.fb.group({
           orderID: [this.orderID, Validators.required],
-        amount: [this.totalAmount, Validators.required],
+          amount: [this.totalAmount, Validators.required],
           source: ['', Validators.required],
           description: ['']
         });
       });
     });
   }
+
   // orderID: [{ value: this.orderID, disabled: true }, Validators.required],
   // totalAmount: [{ value: this.totalAmount, disabled: true }, Validators.required],
 
-  submitPayment(): void {
+  submitPayment() {
     if (this.paymentForm.valid) {
       const paymentData = this.paymentForm.value;
       this.paymentService.createPayment(paymentData).subscribe(
         response => {
           this.success = true;
-          this.message = 'Payment created successfully';
-          console.log('Payment created successfully:', response);
+          this.message = 'Payment successful';
+          console.log('Payment successful:', response);
+          this.paymentForm.reset
         },
         error => {
           this.success = false;
-          this.message = 'Error creating payment: ' + error.message;
+          this.message = 'Error creating payment: Please try again';
           console.error('Error creating payment:', error);
         }
-      );
+      )
     } else {
-      
+      this.success = false;
+      this.message = ' Please fill in all required fields';
     }
   }
-}
+}  
--- Frontend-NG/angular/src/app/components/review/review.component.ts
+++ Frontend-NG/angular/src/app/components/review/review.component.ts
@@ -1,114 +1,83 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ReviewsService } from '../../services/reviews.service';
-import { Review } from '../../intefaces/review.interface';
-import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReviewsService } from '../../services/reviews.service';
 import { AuthServiceService } from '../../services/auth-service.service';
+import { NavbarComponent } from '../navbar/navbar.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NavbarComponent],
   templateUrl: './review.component.html',
   styleUrl: './review.component.css'
 })
 export class ReviewComponent {
-//   private modalService = inject(NgbModal);
-//   closeResult = '';
-//   // Define dp property
-//   dp: any; // You can replace 'any' with the correct type if known
-
-//   open(content: TemplateRef<any>) {
-//     this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-//       (result) => {
-//         this.closeResult = `Closed with: ${result}`;
-//       },
-//       (reason) => {
-//         this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-//       },
-//     );
-//   }
-
-//   private getDismissReason(reason: any): string {
-//     switch (reason) {
-//       case ModalDismissReasons.ESC:
-//         return 'by pressing ESC';
-//       case ModalDismissReasons.BACKDROP_CLICK:
-//         return 'by clicking on a backdrop';
-//       default:
-//         return `with: ${reason}`;
-//     }
-//   }
-// }
-
-// rating: number = 0; // Initialize to 0
-//   reviewText: string = ''; // Initialize to empty string
-
-//   constructor(private router: ActivatedRoute, private reviewService: ReviewsService, private modalService: NgbModal) {}
-
-//   submitReview() {
-//     const reviewData: Review = {
-//       rating: this.rating,
-//       reviewText: this.reviewText,
-//       userID: localStorage.getItem('userID') as string,
-//       orderID: this.router.snapshot.params['id'],
-//     };
-
-//     this.reviewService.createReview(reviewData).subscribe(
-//       response => {
-//         console.log('Review submitted successfully:', response);
-//         // Optionally, you can navigate to a different page or display a success message
-//       },
-//       error => {
-//         console.error('Error submitting review:', error);
-//         // Optionally, you can display an error message to the user
-//       }
-//     );
-
-//     // Reset form fields and close the modal
-//     this.rating = 0;
-//     this.reviewText = '';
-//     this.modalService.dismissAll();
-//   }
-
   reviewForm!: FormGroup;
   isFormVisible = true;
-  constructor(private fb: FormBuilder, private reviewService: ReviewsService, public authService:AuthServiceService) { }
+  orderID: string | null = null; 
+
+  constructor(private fb: FormBuilder, private reviewService: ReviewsService, public authService: AuthServiceService) { }
 
   ngOnInit(): void {
+    this.orderID = localStorage.getItem('orderID'); 
+
     this.reviewForm = this.fb.group({
-      rating: ['', Validators.required],
+      rating: ['', [Validators.required, Validators.min(1), Validators.max(5)]],
       reviewText: ['', Validators.required]
     });
   }
 
   submitReview(): void {
-    if (this.reviewForm.valid) {
+    if (this.reviewForm.valid && this.orderID) { 
       const reviewData = this.reviewForm.value;
-      this.reviewService.createReview(reviewData).subscribe(
-        response => {
-          console.log('Review submitted successfully:', response);
-          const reviewData = this.reviewForm.value;
-          const token = localStorage.getItem('token') as string;
-          this.authService.readToken(token).subscribe(
-            token => {
-              if (token) {
-                reviewData.userID = token.userID;
+      
+      const token = localStorage.getItem('token') as string;
+      this.authService.readToken(token).subscribe(
+        (token) => { 
+          if (token) {
+            reviewData.userID = token.info.userID;
+            reviewData.orderID = this.orderID;
+            this.reviewService.createReview(reviewData).subscribe(
+              response => {
+                Swal.fire({
+                  title: 'Review Submission',
+                  text: 'Your review has been submitted successfully.',
+                  icon: 'success'
+                });
+                this.reviewForm.reset();
+                console.log('Review submitted successfully:', response);
+              },
+              error => {
+                Swal.fire({
+                  title: 'Error',
+                  text:'Review already made for this order',
+                  icon: 'error'
+                });
+                this.reviewForm.reset();
+                console.error('Error submitting review:', error);
               }
-            }
-          )
-         
+            );
+          }
         },
         error => {
-          console.error('Error submitting review:', error);
+          Swal.fire({
+            title: 'Error',
+            text: 'An error occurred while reading token. Please try again later.',
+            icon: 'error'
+          });
+          // this.reviewForm.reset();
+          console.error('Error reading token:', error);
         }
       );
     } else {
+      console.log("error");
     }
   }
+
   closeForm(): void {
     this.isFormVisible = false;
+    this.reviewForm.reset();
   }
 }
